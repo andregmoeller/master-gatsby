d915d86f0c25876fade96d23bf308794c54b657e
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2021-01-13 22:48:44 +0100

    Queries the pizzas

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hey! I'm the Pizzas page</p>
+      <p>Hey! There are {pizzas.length} Pizzas!</p>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
