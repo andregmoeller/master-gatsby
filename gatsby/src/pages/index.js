71db75ea4e2688b458b6925b4b99e940abd86ee1
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2021-01-20 01:58:40 +0100

    Adds site metadata

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import SEO from '../components/SEO';
 
 export default function HomePage() {
   return (
     <>
+      <SEO title="The Best Pizza in Hamilton!" />
       <p>Hey! I'm the home page</p>
     </>
   );
--- gatsby/src/templates/Slicemaster.js
+++ gatsby/src/templates/Slicemaster.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import SEO from '../components/SEO';
 
 export default function SlicemasterPage({ data }) {
   const { slicemaster } = data;
   return (
-    <div className="center">
-      <Img fluid={slicemaster.image.asset.fluid} />
-      <span className="mark">{slicemaster.name}</span>
-      <p>{slicemaster.description}</p>
-    </div>
+    <>
+      <SEO
+        title={slicemaster.name}
+        image={slicemaster.image?.asset?.fluid?.src}
+      />
+      <div className="center">
+        <Img fluid={slicemaster.image.asset.fluid} />
+        <span className="mark">{slicemaster.name}</span>
+        <p>{slicemaster.description}</p>
+      </div>
+    </>
   );
 }
 
