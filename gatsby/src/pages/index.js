8da0716402f0b46bf3061095a9c5c87f3f677b16
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2021-01-24 22:10:39 +0100

    Adds content to the homepage

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import ItemGrid from '../components/ItemGrid';
+import LoadingGrid from '../components/LoadingGrid';
 import SEO from '../components/SEO';
+import { HomePageGrid } from '../styles/Grids';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing({ slicemasters }) {
+  return (
+    <div>
+      <h2>
+        <span className="mark tilt">Slicemasters On</span>
+      </h2>
+      <p>Standing by, ready to slice you up!</p>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && (
+        <p>No one is working right now!</p>
+      )}
+      {slicemasters?.length && <ItemGrid items={slicemasters} />}
+    </div>
+  );
+}
+
+function HotSlices({ hotSlices }) {
+  return (
+    <div>
+      <h2>
+        <span className="mark tilt">Hot Slices</span>
+      </h2>
+      <p>Come on by, buy the slice!!</p>
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices?.length && <p>Nothin' in the Case</p>}
+      {hotSlices?.length && <ItemGrid items={hotSlices} />}
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { slicemasters, hotSlices } = useLatestData();
   return (
-    <>
+    <div className="center">
       <SEO title="The Best Pizza in Hamilton!" />
-      <p>Hey! I'm the home page</p>
-    </>
+      <h1>The Best Pizza in Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <HomePageGrid>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </HomePageGrid>
+    </div>
   );
 }
